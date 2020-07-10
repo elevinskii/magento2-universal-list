1cfbf836b7550dc671d7273aed0289a725180f5a
Evgeny Levinsky <el@ideal-code.ru>
2020-07-10 11:15:19 +0300

    Configuration page controller

---
+++ Controller/Adminhtml/Config/Index.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace Elevinskii\UniversalList\Controller\Adminhtml\Config;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\Model\View\Result\Page as ResultPage;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class Index extends Action implements HttpGetActionInterface
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Elevinskii_UniversalList::universal_lists_config';
+
+    /**
+     * Execute the controller
+     *
+     * @return ResultPage
+     */
+    public function execute(): ResultPage
+    {
+        /** @var ResultPage $resultPage */
+        $resultPage = $this->resultFactory->create($this->resultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('Elevinskii_UniversalList::universal_lists_config');
+        $resultPage->getConfig()->getTitle()->prepend(__('Configuration'));
+
+        return $resultPage;
+    }
+}
