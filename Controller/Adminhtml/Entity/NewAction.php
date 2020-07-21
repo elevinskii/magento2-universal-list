8e772a73341ef17bb8e37aa2849ae9d269362cac
Evgeny Levinsky <el@ideal-code.ru>
2020-07-21 16:41:00 +0300

    Controller for a new list

---
+++ Controller/Adminhtml/Entity/NewAction.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Elevinskii\UniversalList\Controller\Adminhtml\Entity;
+
+use Elevinskii\UniversalList\Controller\Adminhtml\Entity as AbstractEntity;
+use Magento\Backend\Model\View\Result\Page as ResultPage;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class NewAction extends AbstractEntity implements HttpGetActionInterface
+{
+    /**
+     * Execute the controller
+     *
+     * @return ResultPage
+     */
+    public function execute(): ResultPage
+    {
+        /** @var ResultPage $resultPage */
+        $resultPage = $this->resultFactory->create($this->resultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('Elevinskii_UniversalList::universal_lists_entities');
+        $resultPage->getConfig()->getTitle()->prepend(__('New List'));
+
+        return $resultPage;
+    }
+}
