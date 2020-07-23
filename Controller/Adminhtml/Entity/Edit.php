4e4d326cb03f19553e88abe980721dd581b2c071
Evgeny Levinsky <el@ideal-code.ru>
2020-07-23 10:39:33 +0300

    Adjusting edit controller to find needed entity

--- Controller/Adminhtml/Entity/Edit.php
+++ Controller/Adminhtml/Entity/Edit.php
@@ -4,18 +4,70 @@ declare(strict_types=1);
 namespace Elevinskii\UniversalList\Controller\Adminhtml\Entity;
 
 use Elevinskii\UniversalList\Controller\Adminhtml\Entity as AbstractEntity;
+use Elevinskii\UniversalList\Model\EntityFactory as EntityModelFactory;
+use Elevinskii\UniversalList\Model\ResourceModel\EntityFactory as EntityResModelFactory;
+use Magento\Backend\App\Action\Context;
 use Magento\Backend\Model\View\Result\Page as ResultPage;
+use Magento\Backend\Model\View\Result\Redirect as ResultRedirect;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
 
 class Edit extends AbstractEntity implements HttpGetActionInterface
 {
     /**
+     * @var EntityModelFactory
+     */
+    private $entityModelFactory;
+
+    /**
+     * @var EntityResModelFactory
+     */
+    private $entityResModelFactory;
+
+    /**
+     * Edit constructor
+     *
+     * @param Context $context
+     * @param EntityModelFactory $entityModelFactory
+     * @param EntityResModelFactory $entityResModelFactory
+     */
+    public function __construct(
+        Context $context,
+        EntityModelFactory $entityModelFactory,
+        EntityResModelFactory $entityResModelFactory
+    ) {
+        parent::__construct($context);
+
+        $this->entityModelFactory = $entityModelFactory;
+        $this->entityResModelFactory = $entityResModelFactory;
+    }
+
+    /**
      * Execute the controller
      *
-     * @return ResultPage
+     * @return ResultRedirect|ResultPage
      */
-    public function execute(): ResultPage
+    public function execute(): ResultInterface
     {
-        return $this->initResultPage((string) __('Edit List'));
+        /** @var ResultRedirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('*/*');
+
+        $listId = $this->getRequest()->getParam('list_id');
+        if (!$listId) {
+            return $resultRedirect;
+        }
+
+        $entity = $this->entityModelFactory->create();
+        $this->entityResModelFactory->create()->load($entity, $listId);
+
+        if ($entity->isEmpty()) {
+            $this->messageManager->addErrorMessage(
+                __('The list with ID=%1 does not exist.', $listId)
+            );
+            return $resultRedirect;
+        }
+
+        return $this->initResultPage($entity->getTitle());
     }
 }
