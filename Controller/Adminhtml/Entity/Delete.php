c22354afaa567f39eca2e588879c8574f47918d4
Evgeny Levinsky <el@ideal-code.ru>
2020-07-31 09:36:26 +0300

    Delete list controller

---
+++ Controller/Adminhtml/Entity/Delete.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Elevinskii\UniversalList\Controller\Adminhtml\Entity;
+
+use Elevinskii\UniversalList\Controller\Adminhtml\Entity as AbstractEntity;
+use Exception;
+use Magento\Backend\Model\View\Result\Redirect as ResultRedirect;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\Request\Http as Request;
+
+class Delete extends AbstractEntity implements HttpPostActionInterface
+{
+    /**
+     * Execute the controller
+     *
+     * @return ResultRedirect
+     */
+    public function execute(): ResultRedirect
+    {
+        /** @var ResultRedirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('*/*');
+
+        /** @var Request $request */
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            $listId = $request->getPostValue('list_id');
+            $entity = $this->entityModelFactory->create();
+            $resEntity = $this->entityResModelFactory->create();
+
+            if ($listId) {
+                $resEntity->load($entity, $listId);
+                if ($entity->isEmpty()) {
+                    $this->messageManager->addErrorMessage(
+                        __('The list with ID=%1 does not exist.', $listId)
+                    );
+                    return $resultRedirect;
+                }
+
+                try {
+                    $resEntity->delete($entity);
+                    $this->messageManager->addSuccessMessage(__('The list has been deleted.'));
+                } catch (Exception $e) {
+                    $this->messageManager->addExceptionMessage($e, __('An error occurred while deleting the list.'));
+                    $resultRedirect->setPath('*/*/edit', ['list_id' => $listId]);
+                }
+            }
+        }
+
+        return $resultRedirect;
+    }
+}
