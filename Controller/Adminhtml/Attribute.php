c7064df143814b7a49cbdc7ffebbb1e5f9753e45
Evgeny Levinsky <el@ideal-code.ru>
2020-08-04 17:59:51 +0300

    Blank attributes controller

---
+++ Controller/Adminhtml/Attribute.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Elevinskii\UniversalList\Controller\Adminhtml;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\Model\View\Result\Page as ResultPage;
+
+abstract class Attribute extends Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Elevinskii_UniversalList::universal_lists_attributes';
+
+    /**
+     * Initialization of a backend page
+     *
+     * @param string $title
+     * @return ResultPage
+     */
+    protected function initResultPage(string $title = ''): ResultPage
+    {
+        /** @var ResultPage $resultPage */
+        $resultPage = $this->resultFactory->create($this->resultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('Elevinskii_UniversalList::universal_lists_attributes');
+
+        if ($title) {
+            $resultPage->getConfig()->getTitle()->prepend($title);
+        }
+
+        return $resultPage;
+    }
+}
