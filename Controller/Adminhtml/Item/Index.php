c4b238befb939775e6e8c0a7030d846233225470
Evgeny Levinsky <el@ideal-code.ru>
2020-10-14 10:25:57 +0300

    Controller for item grids

---
+++ Controller/Adminhtml/Item/Index.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Elevinskii\UniversalList\Controller\Adminhtml\Item;
+
+use Magento\Backend\Model\View\Result\Page as ResultPage;
+use Magento\Backend\Model\View\Result\Redirect as ResultRedirect;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+
+class Index extends Item implements HttpGetActionInterface
+{
+    /**
+     * Execute the controller
+     *
+     * @return ResultRedirect|ResultPage
+     */
+    public function execute(): ResultInterface
+    {
+        /** @var ResultRedirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('*/entity');
+
+        $listId = $this->getRequest()->getParam('list_id');
+        if (!$listId) {
+            return $resultRedirect;
+        }
+
+        $entity = $this->getEntityModel();
+        $this->getEntityResModel()->load($entity, $listId);
+
+        if ($entity->isEmpty()) {
+            $this->messageManager->addErrorMessage(
+                __('The list with ID=%1 does not exist.', $listId)
+            );
+            return $resultRedirect;
+        }
+
+        return $this->initResultPage([
+            __('Universal Lists'),
+            $entity->getTitle()
+        ]);
+    }
+}
